badbaeaaf05c26b08534881196e2dbfae6cbe73b
sudo-dqr <3577669403@qq.com>
2024-10-19 23:45:12 +0800

    update Gemfile

--- cookbook3/db/schema.rb
+++ cookbook3/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_19_150509) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_19_152044) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_19_150509) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id", null: false
+    t.string "instructions"
     t.index ["category_id"], name: "index_recipes_on_category_id"
   end
 
